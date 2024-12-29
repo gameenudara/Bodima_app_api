cece56c6e5bc13635d0bd811b70e12a0c73c003e
gameenudara <gameenudara567@gmail.com>
2024-12-29 17:42:43 +0530

    git init

---
+++ src/main/java/lk/bodima/api/BodimaApiApplication.java
@@ -0,0 +1,13 @@
+package lk.bodima.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodimaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodimaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/bodima/api/BodimaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.bodima.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodimaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
